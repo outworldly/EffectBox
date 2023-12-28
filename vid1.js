391a87a7e029f97ba4e8d2c70ba02608e80a8837
outworldly <123209409+outworldly@users.noreply.github.com>
2023-12-28 00:34:19 +0000

    Update vid1.js

--- vid1.js
+++ vid1.js
@@ -13,6 +13,8 @@ text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
 var canvas = document.querySelector("canvas");
     var context = canvas.getContext("2d");
     const video = document.querySelector('#vid1');
+    const vid2 = document.querySelector('#vid2');
+    const vid3 = document.querySelector('#vid3');
 	
 	//w-width,h-height
     var w, h;
@@ -22,12 +24,34 @@ var canvas = document.querySelector("canvas");
     window.navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(stream => {
             video.srcObject = stream;
+            vid2.srcObject = stream;
+            vid3.srcObject = stream;
             video.onloadedmetadata = (e) => {
                 video.play();
                 
         
                 w = video.videoWidth;
                 h = video.videoHeight
+                
+                canvas.width = w;
+                canvas.height = h;
+            };
+            vid2.onloadedmetadata = (e) => {
+                vid2.play();
+                
+        
+                w = vid2.videoWidth;
+                h = vid2.videoHeight
+                
+                canvas.width = w;
+                canvas.height = h;
+            };
+            vid3.onloadedmetadata = (e) => {
+                vid3.play();
+                
+        
+                w = vid3.videoWidth;
+                h = vid3.videoHeight
                 
                 canvas.width = w;
                 canvas.height = h;
