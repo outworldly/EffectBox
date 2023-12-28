a9a0f06de9f297639bc5d221cd978bd5b2954822
outworldly <123209409+outworldly@users.noreply.github.com>
2023-12-28 00:27:40 +0000

    Update vid2.js

--- vid2.js
+++ vid2.js
@@ -2,16 +2,15 @@
 // stream video
 var canvas = document.querySelector("canvas");
     var context = canvas.getContext("2d");
-    const video = document.querySelector('#vid1');
+    const video = document.querySelector('#vid2');
+const content = document.querySelector('#vid1');
 	
 	//w-width,h-height
     var w, h;
     canvas.style.display = "none";
 	
 
-    window.navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-        .then(stream => {
-            video.srcObject = stream;
+            video.srcObject = content.srcObject;
             video.onloadedmetadata = (e) => {
                 video.play();
                 
@@ -22,10 +21,8 @@ var canvas = document.querySelector("canvas");
                 canvas.width = w;
                 canvas.height = h;
             };
-    	})
-        .catch(error => {
-        	alert('You have to enable the mic and the camera');
-    	});
+
+
 // hide controls FireFox
 var vids = $("video"); 
 $.each(vids, function(){
