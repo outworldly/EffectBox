90b12f77df8a0e4275143d49c255a3b9616bbdd4
outworldly <123209409+outworldly@users.noreply.github.com>
2024-01-01 23:23:33 +0000

    Update filters.js

--- filters.js
+++ filters.js
@@ -1,16 +1,1 @@
 
- filters = ['blur','inverse','convolve','convoblur','convolve2','blackandwhite','noir','bluefill','displacement'],
- i = 1;
-v = 4;
-while (v > 0){
-var vid = '#vid',[i]
-var video = document.querySelector(vid);
-  current.innerHTML = filters[i];
-  video.style.webkitFilter='url(#'+filters[i]+')';
-  video.style.mozFilter='url(#'+filters[i]+')';
-  video.style.filter='url(#'+filters[i]+')';
-vid = vid-1
-
- i++;
-  
-};
