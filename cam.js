21f50ef14f59d0ef02f59c6861bfa7da63ebcaf9
outworldly <123209409+outworldly@users.noreply.github.com>
2024-01-01 23:38:20 +0000

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,13 +1,4 @@
-// fetch ip
-function text(url) {
-  return fetch(url).then(res => res.text());
-}
 
-text('https://www.cloudflare.com/cdn-cgi/trace').then(data => {
-  let ipRegex = /[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/
-  let ip = data.match(ipRegex)[0];
-  console.log(ip);
-});
 
 // stream video
 var canvas = document.querySelector("canvas");
