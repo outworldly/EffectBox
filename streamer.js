1df0dd6a56c7810e1bc8de4e0d10964bd31c336c
outworldly <123209409+outworldly@users.noreply.github.com>
2024-01-02 00:50:24 +0000

    Create streamer.js

---
+++ streamer.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Streamer Mode
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-02
+// @description  Hide your IP for streamers
+// @author       Outworldly
+// @match        https://outworldly.github.io/EffectBox/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=github.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementById('greet').id = 'streamer';
+    document.getElementById("streamer").textContent="Streamer's Effect Box";
+
+})();
